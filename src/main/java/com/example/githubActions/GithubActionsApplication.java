6eacbb0aaf6e2ccb78f870d7cb5f8ec5085fd587
Your Name <your_email@example.com>
2025-11-03 12:06:59 +0530

    second commit

--- src/main/java/com/example/githubActions/GithubActionsApplication.java
+++ src/main/java/com/example/githubActions/GithubActionsApplication.java
@@ -2,9 +2,17 @@ package com.example.githubActions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubActionsApplication {
+
+	@GetMapping
+	public String hello(){
+		return "Hello world !";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsApplication.class, args);
